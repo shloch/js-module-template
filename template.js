91508aed8a27ccc034245426704433f9be4e2c38
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2020-01-08 18:15:17 +0100

    first commit

---
+++ template.js
@@ -0,0 +1,16 @@
+var Module = (function () {
+  var myModule = {};
+  var privateMethod = function () {
+
+  };
+  myModule.publicMethod = function () {
+
+  };
+  myModule.anotherPublicMethod = function () {
+
+  };
+  return myModule; // returns the Object with public methods
+})();
+
+// usage
+Module.publicMethod();
