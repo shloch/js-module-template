0798344ddfdf5df3c965a84883dd35c8d69f82bd
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2020-01-08 19:09:24 +0100

    factory + module

---
+++ template_factory.js
@@ -0,0 +1,32 @@
+//public methods can access private methods
+
+const Player = (name, level) => {
+  let health = level * 2; //health s private
+  const getLevel = () => level;
+  const getName = () => name;
+  const die = () => {
+    // uh oh
+  };
+  const damage = x => {
+    health -= x;
+    if (health <= 0) {
+      die();
+    }
+  };
+  const attack = enemy => {
+    if (level < enemy.getLevel()) {
+      damage(1);
+      console.log(`${enemy.getName()} has damaged ${name}`);
+    }
+    if (level >= enemy.getLevel()) {
+      enemy.damage(1);
+      console.log(`${name} has damaged ${enemy.getName()}`);
+    }
+  };
+  return { attack, damage, getLevel, getName } //only returned methods are publicly available
+};
+
+//usage
+const jimmie = Player('jim', 10);
+const badGuy = Player('jeff', 5);
+jimmie.attack(badGuy);
